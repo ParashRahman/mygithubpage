148412475e55e12b9470c96e8354e3d6bb7ffa1b
Parash Rahman <parash@ualberta.ca>
2019-09-06 14:20:21 -0600

    wip

---
+++ blogpage.js
@@ -0,0 +1,6 @@
+var blogposts = ['rap', 'sample1', 'sample2'];
+var blogpage = `
+<div class='blogpage'>
+  <button class='blogpost' id='bp-'
+</div>
+`;
---
+++ index.js
@@ -0,0 +1,8 @@
+function showpage(name) {
+  if (name == 'blog') {
+    
+  }
+  else if (name == 'schedule') {
+    document.getElementsByClassName('maincontent')[0].innerHTML='<iframe src="https://calendar.google.com/calendar/embed?src=ualberta.ca_79j51oukmvuf46u8gssppu3er4%40group.calendar.google.com&ctz=America%2FEdmonton" style="border: 0" width="800" height="600" frameborder="0" scrolling="no"></iframe>'
+  }
+}
